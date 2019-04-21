396bea5baace5c59c6e5b47305daf17a65720a80
Eduardo L. Martinez <eduardo.martinez@nbcuni.com>
2019-04-21 19:23:33 -0400

    Finishing off the install script

--- local_init.sh
+++ local_init.sh
@@ -10,35 +10,35 @@ sudo -v
 
 echo '😈 Spinning up Docker!'
 docker-compose up -d
-sleep 120
+# sleep 120
 
-echo '😈 Removing WordPress wp-content folder'
-rm -Rf wp-container/wp-content
+# echo '😈 Removing WordPress wp-content folder'
+# rm -Rf wp-container/wp-content
 
-echo 'Replacing it with our VIP NBCOTS Repository'
-git clone $PROJECT_REPOSITORY_SSH_URL --recursive wp-container/wp-content/
+# echo 'Replacing it with our VIP NBCOTS Repository'
+# git clone $PROJECT_REPOSITORY_SSH_URL --recursive wp-container/wp-content/
 
-# Check to see if this is a VIP Installation
-if $IS_VIP_ENV
-then
-    git clone -q https://github.com/Automattic/vip-go-mu-plugins.git --recursive wp-container/wp-content/mu-plugins
-fi
+# # Check to see if this is a VIP Installation
+# if $IS_VIP_ENV
+# then
+#     git clone -q https://github.com/Automattic/vip-go-mu-plugins.git --recursive wp-container/wp-content/mu-plugins
+# fi
 
-echo '😈 Install node things and stealing your bank account information'
-cd ./wp-container/wp-content
+# echo '😈 Install node things and stealing your bank account information'
+# cd ./wp-container/wp-content
 
-echo "😈 Building composer. Honeslty have no idea what this is for."
-composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+# echo "😈 Building composer. Honeslty have no idea what this is for."
+# composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 
-echo '😈 Run all the scripts! For node... I think...'
-chmod +x bin/script-build.sh
-echo '😈 Building plugin byline-manager'
-cd plugins/byline-manager && npm install --quiet && npm run build && cd ..
-echo '😈 Building plugin nbc-library'
-cd nbc-library && npm install --quiet && npm run build && cd .. && cd ..
-echo '😈 Building theme nbc-station'
-nvm install 8
-npm i -g npm@6
-cd themes/nbc-station && npm install --quiet && npm run build
+# echo '😈 Run all the scripts! For node... I think...'
+# chmod +x bin/script-build.sh
+# echo '😈 Building plugin byline-manager'
+# cd plugins/byline-manager && npm install --quiet && npm run build && cd ..
+# echo '😈 Building plugin nbc-library'
+# cd nbc-library && npm install --quiet && npm run build && cd .. && cd ..
+# echo '😈 Building theme nbc-station'
+# nvm install 8
+# npm i -g npm@6
+# cd themes/nbc-station && npm install --quiet && npm run build
 
 echo 'Done. Maybe..'  

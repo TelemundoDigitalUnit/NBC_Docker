494c1c001c39f9d7f219e444ea74f676d9187342
Eduardo L. Martinez <eduardo.martinez@nbcuni.com>
2019-06-21 15:56:27 -0400

    Uploading a .prereqs setup script

---
+++ .prereqs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# php
+brew install php
+
+# composer
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
+
+# npm
+brew install npm
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+mkdir .nvm
+nvm install 8

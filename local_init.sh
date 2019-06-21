39e265bbb824eccd2630a744bf02abe232668025
Eduardo L. Martinez <eduardo.martinez@nbcuni.com>
2019-06-21 14:59:43 -0400

    Cleaned up sh file installation

--- local_init.sh
+++ local_init.sh
@@ -5,8 +5,8 @@ source .env
 echo '😈 Give me all your base!'
 
 # Let get the password upfront what we need to do below!
-echo '😈 I promise not to share your password with 3rd parties.. or Russia'
-sudo -v
+# echo '😈 I promise not to share your password with 3rd parties.. or Russia'
+# sudo -v
 
 echo '😈 Spinning up Docker and then taking a 30s nap!'
 docker-compose up -d
@@ -16,13 +16,10 @@ echo '😈 Removing WordPress wp-content folder'
 rm -Rf wp-container/wp-content
 
 echo 'Replacing it with our VIP NBCOTS Repository'
-git clone $PROJECT_REPOSITORY_SSH_URL --recursive wp-container/wp-content/
+git clone https://github.com/wpcomvip/nbcots.git wp-container/wp-content/ --branch develop --recurse-submodules
 
-# Check to see if this is a VIP Installation
-if $IS_VIP_ENV
-then
-    git clone -q https://github.com/Automattic/vip-go-mu-plugins.git --recursive wp-container/wp-content/mu-plugins
-fi
+echo 'Pulling VIP MU Plugins'
+git clone --recurse-submodules https://github.com/Automattic/vip-go-mu-plugins-built wp-container/wp-content/mu-plugins
 
 echo '😈 Install node things and stealing your bank account information'
 cd ./wp-container/wp-content
@@ -30,18 +27,16 @@ cd ./wp-container/wp-content
 echo "😈 Building composer. Honeslty have no idea what this is for."
 composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 
-echo '😈 Run all the scripts! For node... I think...'
-# chmod +x bin/script-build.sh
-
 echo '😈 Building plugin byline-manager'
 cd plugins/byline-manager
 npm install --quiet
-npm run build && cd ..
+npm run build
+cd ..
 
 echo '😈 Building plugin nbc-library'
 cd nbc-library
 npm install --quiet
-npm run build
+npm run build -s
 cd ..
 cd ..
 
@@ -50,6 +45,6 @@ nvm install 8
 npm i -g npm@6
 cd themes/nbc-station
 npm install --quiet
-npm run build
+npm run build -s
 
 echo 'Done. Maybe..'  

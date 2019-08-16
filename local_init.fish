27c70215a174cdaaceac504108e0965560f5a7e5
Steve Birstok <Steve.Birstok@nbcuni.com>
2019-08-16 12:44:45 -0400

    update local_init scripts to be more stable

--- local_init.fish
+++ local_init.fish
@@ -2,56 +2,136 @@
 # Load Environment Variables
 cat .env | egrep -v '^(#.*)?$' | sed 's/^/set /g' | sed 's/=/ /g' | source
 
-# Set the base path
+# define variables
+set -l iterations 0
+set -l max_iterations 5
 set -l BASE_SCRIPT_PATH (pwd)
+set -lx LOG_FILE $BASE_SCRIPT_PATH/build_log.txt
 set -l SETUP_PATH $BASE_SCRIPT_PATH/setup
+
+# Colors
+set -l NC "\033[0m" # No Color
+set -l RED "\033[0;31m"
+set -l GREEN "\033[0;32m"
+
+# check for errors
+function check_error
+    if [ $argv -ne 0 ]
+        printf "$RED\x00An error occurred, check $LOG_FILE\n$NC"
+        exit
+    end
+end
+
+# clear the error log
+echo -n > $LOG_FILE
 
 # Get parameters
 source $SETUP_PATH/fish/getopts.fish $argv
 
-echo "😈 Compiling project: $PROJECT"
+check_error $status
 
-# Let get the password upfront what we need to do below!
-# echo '😈 I promise not to share your password with 3rd parties.. or Russia'
-# sudo -v
+# Output to the user which project will be built
+printf "$GREEN😈 Compiling project: $PROJECT$NC\n"
+printf "[ this may take a minute ... ]\n"
 
-echo '😈 Spinning up Docker and then taking a nap until state is running..'
-docker-compose up -d 2>/dev/null &
+docker-compose build 1>>$LOG_FILE 2>>$LOG_FILE
 
-while test (docker inspect -f '{{.State.Running}}' nbc-wp-php 2>/dev/null) != "true"
-    echo -n '.'
-    sleep 0.1
+check_error $status
+
+# spin up mysql
+printf "$GREEN\x00Spinning up MySQL..\n$NC"
+docker-compose up -d mysql 1>>$LOG_FILE 2>>$LOG_FILE
+
+check_error $status
+
+# wait for mysql to be "ready"
+set iterations 1
+while true
+    docker-compose exec mysql /bin/sh -c "mysqlshow -p\$MYSQL_ROOT_PASSWORD" 1>>$LOG_FILE 2>>$LOG_FILE
+
+    if [ $status -eq 0 ]
+        break
+    end
+
+    printf '.'
+
+    if [ $iterations -ge $max_iterations ]
+        printf "$RED\nMySQL failed to come up after $max_iterations attempts.\n$NC"
+        check_error 1
+    end
+
+    set iterations (expr $iterations + 1)
 end
 
-echo
+# spin up wordpress
+printf "$GREEN\x00Spinning up Wordpress..\n$NC"
+docker-compose up -d wordpress 1>>$LOG_FILE 2>>$LOG_FILE
 
-echo '😈 Removing WordPress wp-content folder'
+check_error $status
+
+# wait for wordpress to be "ready"
+set iterations 1
+while true
+    docker-compose exec wordpress /bin/sh -c "env SCRIPT_NAME=/var/www/html/wp-admin/index.php SCRIPT_FILENAME=/var/www/html/wp-admin/index.php REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000" 1>>$LOG_FILE 2>>$LOG_FILE
+
+    if [ $status -eq 0 ]
+        break
+    end
+
+    printf '.'
+
+    if [ $iterations -ge $max_iterations ]
+        printf "$RED\nWordpress failed to come up after $max_iterations attempts.\n$NC"
+        check_error 1
+    end
+
+    set iterations (expr $iterations + 1)
+end
+
+printf "$GREEN😈 Removing WordPress wp-content folder$NC\n"
 rm -Rf wp-container/wp-content
 
 set -l PROJECT_REPOSITORY_SSH_URL ""
 
 if [ "$PROJECT" = "" -o "$PROJECT" = "main" ];
-    set PROJECT_REPOSITORY_SSH_URL https://github.com/wpcomvip/nbcots.git ;
-else if [ "$PROJECT" = "lx" ];
-    set PROJECT_REPOSITORY_SSH_URL https://github.com/wpcomvip/nbcotslx.git ;
+    set PROJECT_REPOSITORY_SSH_URL git@github.com-nbcsteveb:wpcomvip/nbcots.git ;
+else if [ "$PROJECT" = "lx" -o "$PROJECT" = "localx" ];
+    set PROJECT_REPOSITORY_SSH_URL git@github.com-nbcsteveb:wpcomvip/nbcotslx.git ;
 end
 
-echo '😈 Replacing it with our VIP NBCOTS Repository'
-git clone $PROJECT_REPOSITORY_SSH_URL wp-container/wp-content/ --branch $DEFAULT_BRANCH --recurse-submodules
+printf "$GREEN😈 Replacing it with our VIP NBCOTS Repository$NC\n"
+printf "Repo URL: %s\nBranch: %s\n" $PROJECT_REPOSITORY_SSH_URL $DEFAULT_BRANCH
+printf "[ this may take a minute ... ]\n"
+git clone $PROJECT_REPOSITORY_SSH_URL wp-container/wp-content/ --branch $DEFAULT_BRANCH --recurse-submodules 1>>$LOG_FILE 2>>$LOG_FILE
 
-echo '😈 Pulling VIP MU Plugins'
-git clone --recurse-submodules https://github.com/Automattic/vip-go-mu-plugins-built wp-container/wp-content/mu-plugins
+check_error $status
 
-command -v composer >/dev/null 2>&1
+printf "$GREEN😈 Pulling VIP MU Plugins\n$NC"
+printf "[ this may take a minute ... ]\n"
+git clone --recurse-submodules https://github.com/Automattic/vip-go-mu-plugins-built wp-container/wp-content/mu-plugins 1>>$LOG_FILE 2>>$LOG_FILE
+
+check_error $status
+
+command -v composer 1>>$LOG_FILE 2>>$LOG_FILE
 if [ $status -eq 0 ]
     pushd "$BASE_SCRIPT_PATH/wp-container/wp-content"
-        echo "😈 Building composer. Honeslty have no idea what this is for."
+        printf "$GREEN😈 Building composer. Honeslty have no idea what this is for.\n$NC"
         composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
     popd
 end
 
-echo '😈 Install node things and stealing your bank account information'
+printf "$GREEN😈 Installing multi-site support..\n$NC"
+docker-compose run wp-cli 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $status
 
+docker-compose run wp-cli 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $status
+
+printf "$GREEN😈 Spinning up nginx and phpmyadmin\n$NC"
+docker-compose up -d nginx phpmyadmin 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $status
+
+printf "$GREEN😈 Install node things and stealing your bank account information\n$NC"
 nvm use 8
 npm i -g npm@6
 
@@ -63,4 +143,4 @@ else if [ -e "$SETUP_PATH/generic/$PROJECT.sh" ];
     source "$SETUP_PATH/generic/$PROJECT.sh"
 end
 
-echo '😈 Done. Maybe..'
+printf "$GREEN😈 Done. Maybe..\n$NC"
--- local_init.sh
+++ local_init.sh
@@ -2,63 +2,145 @@
 # Load Environment Variables
 source .env
 
-# Set the base path
+# define variables
+iterations=0
+max_iterations=5
 BASE_SCRIPT_PATH=$(pwd)
-SETUP_PATH="${BASE_SCRIPT_PATH}/setup"
+LOG_FILE=${BASE_SCRIPT_PATH}/build_log.txt
+SETUP_PATH=${BASE_SCRIPT_PATH}/setup
+
+# Colors
+NC="\033[0m" # No Color
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+
+# check for errors
+function check_error () {
+    if [ $1 -ne 0 ]; then
+        printf "$RED\x00An error occurred, check $LOG_FILE\n$NC"
+        exit
+    fi
+}
+
+# clear the error log
+echo -n > $LOG_FILE
 
 # Get parameters
 source ${SETUP_PATH}/bash/getopts.sh
 
-echo "😈 Compiling project: ${PROJECT}"
+check_error $?
 
-# Let get the password upfront what we need to do below!
-# echo '😈 I promise not to share your password with 3rd parties.. or Russia'
-# sudo -v
+# Output to the user which project will be built
+printf "$GREEN\x00😈 Compiling project: $PROJECT$NC\n"
+printf "[ this may take a minute ... ]\n"
 
-echo '😈 Spinning up Docker and then taking a nap until state is running..'
-docker-compose up -d 2>/dev/null &
+docker-compose build 1>>$LOG_FILE 2>>$LOG_FILE
 
-until [ "$(docker inspect -f {{.State.Running}} nbc-wp-php 2>/dev/null)" == "true" ]; do
-    echo -n '.'
-    sleep 0.1;
+check_error $?
+
+# spin up mysql
+printf "$GREEN\x00Spinning up MySQL..\n$NC"
+docker-compose up -d mysql 1>>$LOG_FILE 2>>$LOG_FILE
+
+check_error $?
+
+# wait for mysql to be "ready"
+iterations=1
+while true; do
+    docker-compose exec mysql /bin/sh -c "mysqlshow -p\$MYSQL_ROOT_PASSWORD" 1>>$LOG_FILE 2>>$LOG_FILE
+
+    if [ $? -eq 0 ]; then
+        break
+    fi
+
+    printf '.'
+
+    if [ $iterations -ge $max_iterations ]; then
+        printf "$RED\nMySQL failed to come up after $max_iterations attempts.\n$NC"
+        check_error 1
+    fi
+
+    iterations=$(expr $iterations + 1)
 done;
 
-echo
+# spin up wordpress
+printf "$GREEN\x00Spinning up Wordpress..\n$NC"
+docker-compose up -d wordpress 1>>$LOG_FILE 2>>$LOG_FILE
 
-echo '😈 Removing WordPress wp-content folder'
+check_error $?
+
+# wait for wordpress to be "ready"
+iterations=1
+while true; do
+    docker-compose exec wordpress /bin/sh -c "env SCRIPT_NAME=/var/www/html/wp-admin/index.php SCRIPT_FILENAME=/var/www/html/wp-admin/index.php REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000" 1>>$LOG_FILE 2>>$LOG_FILE
+
+    if [ $? -eq 0 ]; then
+        break
+    fi
+
+    printf '.'
+
+    if [ $iterations -ge $max_iterations ]; then
+        printf "$RED\nWordpress failed to come up after $max_iterations attempts.\n$NC"
+        check_error 1
+    fi
+
+    iterations=$(expr $iterations + 1)
+done
+
+printf "$GREEN\x00😈 Removing WordPress wp-content folder$NC\n"
 rm -Rf wp-container/wp-content
 
-if [ "${PROJECT}" = "" -o "${PROJECT}" = "main" ]; then
-    PROJECT_REPOSITORY_SSH_URL=https://github.com/wpcomvip/nbcots.git ;
-elif [ "${PROJECT}" = "lx" ]; then
-    PROJECT_REPOSITORY_SSH_URL=https://github.com/wpcomvip/nbcotslx.git ;
-fi;
+PROJECT_REPOSITORY_SSH_URL=""
 
-echo '😈 Replacing it with our VIP NBCOTS Repository'
-git clone ${PROJECT_REPOSITORY_SSH_URL} wp-container/wp-content/ --branch ${DEFAULT_BRANCH} --recurse-submodules
+if [ "$PROJECT" = "" -o "$PROJECT" = "main" ]; then
+    PROJECT_REPOSITORY_SSH_URL=git@github.com-nbcsteveb:wpcomvip/nbcots.git
+elif [ "$PROJECT" = "lx" -o "$PROJECT" = "localx" ]; then
+    PROJECT_REPOSITORY_SSH_URL=git@github.com-nbcsteveb:wpcomvip/nbcotslx.git
+fi
 
-echo '😈 Pulling VIP MU Plugins'
-git clone --recurse-submodules https://github.com/Automattic/vip-go-mu-plugins-built wp-container/wp-content/mu-plugins
+printf "$GREEN\x00😈 Replacing it with our VIP NBCOTS Repository$NC\n"
+printf "Repo URL: %s\nBranch: %s\n" $PROJECT_REPOSITORY_SSH_URL $DEFAULT_BRANCH
+printf "[ this may take a minute ... ]\n"
+git clone $PROJECT_REPOSITORY_SSH_URL wp-container/wp-content/ --branch $DEFAULT_BRANCH --recurse-submodules 1>>$LOG_FILE 2>>$LOG_FILE
 
-command -v composer >/dev/null 2>&1
+check_error $?
+
+printf "$GREEN\x00😈 Pulling VIP MU Plugins\n$NC"
+printf "[ this may take a minute ... ]\n"
+git clone --recurse-submodules https://github.com/Automattic/vip-go-mu-plugins-built wp-container/wp-content/mu-plugins 1>>$LOG_FILE 2>>$LOG_FILE
+
+check_error $?
+
+command -v composer 1>>$LOG_FILE 2>>$LOG_FILE
 if [ $? -eq 0 ]; then
-    pushd "${BASE_SCRIPT_PATH}/wp-container/wp-content"
-        echo "😈 Building composer. Honeslty have no idea what this is for."
+    pushd "$BASE_SCRIPT_PATH/wp-container/wp-content"
+        printf "$GREEN\x00😈 Building composer. Honeslty have no idea what this is for.\n$NC"
         composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
     popd
 fi
 
-echo '😈 Install node things and stealing your bank account information'
+printf "$GREEN\x00😈 Installing multi-site support..\n$NC"
+docker-compose run wp-cli 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $?
 
+docker-compose run wp-cli 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $?
+
+printf "$GREEN\x00😈 Spinning up nginx and phpmyadmin\n$NC"
+docker-compose up -d nginx phpmyadmin 1>>$LOG_FILE 2>>$LOG_FILE
+check_error $?
+
+printf "$GREEN\x00😈 Install node things and stealing your bank account information\n$NC"
 nvm use 8
 npm i -g npm@6
 
 if [ "$SETUP_SCRIPT" != "" -a -e "$SETUP_SCRIPT" ]; then
     source "$SETUP_SCRIPT"
-elif [ -e "${SETUP_PATH}/bash/${PROJECT}.sh" ]; then
-    source "${SETUP_PATH}/bash/${PROJECT}.sh"
-elif [ -e "${SETUP_PATH}/generic/${PROJECT}.sh" ]; then
-    source "${SETUP_PATH}/generic/${PROJECT}.sh"
+elif [ -e "$SETUP_PATH/bash/$PROJECT.sh" ]; then
+    source "$SETUP_PATH/bash/$PROJECT.sh"
+elif [ -e "$SETUP_PATH/generic/$PROJECT.sh" ]; then
+    source "$SETUP_PATH/generic/$PROJECT.sh"
 fi
 
-echo '😈 Done. Maybe..'
+printf "$GREEN\x00😈 Done. Maybe..\n$NC"

9b2885a13a1ccc67b79515eff8d4c3a9c0fceb79
Steve Birstok <steve.birstok@nbcuni.com>
2019-09-12 16:20:45 -0400

    unit test dockerfile

---
+++ run_tests.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# load variables from .env file
+export $(cat unit_test.env | sed 's/#.*$//g' | xargs)
+
+# Don't change unless you're absolute sure of what you're doing.
+TEST_PATH=/tmp/wordpress-tests-lib
+
+# Functions
+
+create_network() {
+    docker network ls -q -f name="${TEST_NETWORK}" --format="{{.Name}}" | grep "${TEST_NETWORK}" 1>/dev/null
+
+    if [ $? -ne 0 ]; then
+        docker network create "${TEST_NETWORK}";
+    fi
+}
+
+run_mysql() {
+    # Check if the test database container is running
+    docker ps -q --format="{{.Names}}" | grep "${MYSQL_CONTAINER_NAME}" 1>/dev/null
+
+    # If not, start it
+    if [ $? -ne 0 ]; then
+        docker run \
+            --rm \
+            --network="${TEST_NETWORK}" \
+            --network-alias="${MYSQL_CONTAINER_NAME}" \
+            -d \
+            -e MYSQL_DATABASE="${TEST_DB}" \
+            -e MYSQL_ROOT_PASSWORD="${TEST_DB_ROOT_PWD}" \
+            -p "${LOCAL_PORT}:${CONTAINER_PORT}" \
+            --name="${MYSQL_CONTAINER_NAME}" \
+            "${DB_IMAGE}" \
+        ; 1>/dev/null
+    fi
+
+    # Wait for it to really be up
+    TIMES_WAITED=0
+    while [ true ]; do
+        docker exec -it "${MYSQL_CONTAINER_NAME}" /bin/sh -c "echo 'SHOW DATABASES;' | mysql -p'${TEST_DB_ROOT_PWD}' '${TEST_DB}'" 1>/dev/null
+        if [ $TIMES_WAITED -gt ${MAX_WAITS} ]; then
+            echo "mysql container took too long to come up."
+            exit 1
+        fi
+
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        echo -n '.'
+        TIMES_WAITED=`expr $TIMES_WAITED + 1`
+        sleep ${TIME_TO_WAIT}
+    done
+    echo
+};
+
+run_phpunit() {
+    docker build \
+        -f unit_test.Dockerfile \
+        --network="${TEST_NETWORK}" \
+        --build-arg DEFAULT_BRANCH="${DEFAULT_BRANCH}" \
+        --build-arg GIT_USER="${GIT_USER}" \
+        --build-arg GIT_TOKEN="${GIT_TOKEN}" \
+        --build-arg TEST_DB="${TEST_DB}" \
+        --build-arg TEST_DB_ROOT_PWD="${TEST_DB_ROOT_PWD}" \
+        --build-arg TEST_DB_NAME="${MYSQL_CONTAINER_NAME}" \
+        -t "${UNIT_TEST_BASE_IMAGE}" \
+        . \
+    ;
+
+    docker run \
+        --rm \
+        -it \
+        --network="${TEST_NETWORK}" \
+        --network-alias="${UNIT_TEST_CONTAINER_NAME}" \
+        --name="${UNIT_TEST_CONTAINER_NAME}" \
+        "${UNIT_TEST_BASE_IMAGE}" \
+    ;
+};
+
+main() {
+    create_network
+    run_mysql
+    run_phpunit
+};
+
+# Run it
+main
+echo "Done"
---
+++ unit_test.Dockerfile
@@ -0,0 +1,62 @@
+FROM wordpress
+
+RUN apt-get update && \
+    apt-get install -y \
+        wget \
+        git \
+        subversion \
+        mariadb-client \
+    ;
+
+# RUN useradd foo && \
+#     mkdir /home/foo && \
+#     chown -hR foo:foo /home/foo
+
+# WORKDIR /home/foo
+# USER foo
+
+ENV PATH="/opt/nbc/bin:${PATH}"
+
+# install phpunit (see: https://make.wordpress.org/core/handbook/testing/automated-testing/phpunit/)
+RUN mkdir -p /opt/nbc/bin && \
+    wget https://phar.phpunit.de/phpunit-7.5.phar && \
+    chmod +x phpunit-7.5.phar && \
+    mv phpunit-7.5.phar /opt/nbc/bin/phpunit && \
+    phpunit --version
+
+# install composer
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
+    php composer-setup.php && \
+    php -r "unlink('composer-setup.php');" && \
+    mv composer.phar /opt/nbc/bin/composer
+
+RUN rm -rf /usr/src/wordpress/wp-content
+
+ARG DEFAULT_BRANCH
+ARG GIT_USER
+ARG GIT_TOKEN
+ARG TEST_DB
+ARG TEST_DB_ROOT_PWD
+ARG TEST_DB_NAME
+
+RUN git clone \
+    --branch "${DEFAULT_BRANCH}" \
+    --recurse-submodules \
+    "https://${GIT_USER}:${GIT_TOKEN}@github.com/wpcomvip/nbcots.git" \
+    /usr/src/wordpress/wp-content
+
+RUN git clone \
+    --recurse-submodules \
+    "https://${GIT_USER}:${GIT_TOKEN}@github.com/Automattic/vip-go-mu-plugins-built" \
+    /usr/src/wordpress/wp-content/mu-plugins
+
+WORKDIR /usr/src/wordpress/wp-content
+
+RUN bash bin/install-wp-tests.sh "${TEST_DB}" root "${TEST_DB_ROOT_PWD}" "${TEST_DB_NAME}" latest true
+
+RUN composer install
+
+WORKDIR /usr/src/wordpress/wp-content/themes/nbc-station
+
+CMD [ "phpunit" ]
